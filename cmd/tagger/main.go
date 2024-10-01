3073623bc21806cb3b04942e9c9cfa8d2b1aba11
nylon <dorlneylon@gmail.com>
2024-10-01 10:49:06 +0300

    fix(catboost): synced with tf-idf settings

--- cmd/tagger/main.go
+++ cmd/tagger/main.go
@@ -6,13 +6,79 @@ import (
 	"math"
 	"os"
 	"strings"
+	"unicode"
 
 	cb "github.com/mirecl/catboost-cgo/catboost"
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
 )
 
 type TfIdfData struct {
 	Vocabulary map[string]int `json:"vocabulary"`
 	IdfValues  []float64      `json:"idf_values"`
+}
+
+func stripAccents(input string) string {
+	t := transform.Chain(norm.NFD, transform.RemoveFunc(func(r rune) bool {
+		return unicode.Is(unicode.Mn, r)
+	}), norm.NFC)
+	output, _, _ := transform.String(t, input)
+	return output
+}
+
+func charNGrams(input string, ngramRange [2]int) []string {
+	ngrams := []string{}
+	words := strings.Fields(input)
+	for _, word := range words {
+		runes := []rune(" " + word + " ")
+		for n := ngramRange[0]; n <= ngramRange[1]; n++ {
+			for i := 0; i < len(runes)-n+1; i++ {
+				ngrams = append(ngrams, string(runes[i:i+n]))
+			}
+		}
+	}
+	return ngrams
+}
+
+func sublinearTermFrequency(term string, document string) float64 {
+	count := 0
+	ngrams := charNGrams(document, [2]int{1, 3})
+	for _, ngram := range ngrams {
+		if ngram == term {
+			count++
+		}
+	}
+	if count > 0 {
+		return 1 + math.Log(float64(count))
+	}
+	return 0
+}
+
+func CalculateTfIdfVector(rateName string, tfidfData TfIdfData) []float32 {
+	preprocessed := strings.ToLower(rateName)
+	preprocessed = stripAccents(preprocessed)
+
+	_ = charNGrams(preprocessed, [2]int{1, 3})
+	vector := make([]float32, len(tfidfData.Vocabulary))
+
+	for term, index := range tfidfData.Vocabulary {
+		tf := sublinearTermFrequency(term, preprocessed)
+		idf := tfidfData.IdfValues[index]
+		vector[index] = float32(tf * idf)
+	}
+
+	norm := float32(0.0)
+	for _, v := range vector {
+		norm += v * v
+	}
+	norm = float32(math.Sqrt(float64(norm)))
+	if norm > 0 {
+		for i := range vector {
+			vector[i] /= norm
+		}
+	}
+
+	return vector
 }
 
 func LoadTfIdfData(filePath string) (TfIdfData, error) {
@@ -28,27 +94,6 @@ func LoadTfIdfData(filePath string) (TfIdfData, error) {
 	return data, nil
 }
 
-func TermFrequency(term string, document string) float64 {
-	words := strings.Fields(document)
-	termCount := 0
-	for _, word := range words {
-		if word == term {
-			termCount++
-		}
-	}
-	return float64(termCount) / float64(len(words))
-}
-
-func CalculateTfIdfVector(rateName string, tfidfData TfIdfData) []float32 {
-	vector := make([]float32, len(tfidfData.Vocabulary))
-	for term, index := range tfidfData.Vocabulary {
-		tf := TermFrequency(term, rateName)
-		idf := tfidfData.IdfValues[index]
-		vector[index] = float32(tf * idf)
-	}
-	return vector
-}
-
 func LoadModelAndPredict(rateName string, modelPath string, tfidfData TfIdfData, labels []string) (string, error) {
 	model, err := cb.LoadFullModelFromFile(modelPath)
 
@@ -57,6 +102,7 @@ func LoadModelAndPredict(rateName string, modelPath string, tfidfData TfIdfData,
 	}
 
 	vector := CalculateTfIdfVector(rateName, tfidfData)
+	fmt.Println(vector)
 	res, err := model.Predict([][]float32{vector}, [][]string{})
 
 	if err != nil {
@@ -97,15 +143,15 @@ func argmax(values []float64) int {
 }
 
 func main() {
-	tfidfData, err := LoadTfIdfData("tfidf_data.json")
+	tfidfData, err := LoadTfIdfData("data/tfidf/tfidf_data.json")
 	if err != nil {
 		fmt.Printf("Error loading TF-IDF data: %v\n", err)
 		return
 	}
 
-	rateName := "deluxe triple room"
-	modelPath := "catboost_model_class.cbm"
-	classLabels, err := LoadLabels("class_labels.json")
+	rateName := "King Premium Mountain View no balcony"
+	modelPath := "data/cbm/catboost_model_quality.cbm"
+	classLabels, err := LoadLabels("data/labels/labels_quality.json")
 
 	if err != nil {
 		fmt.Printf("Error loading class labels: %v\n", err)
