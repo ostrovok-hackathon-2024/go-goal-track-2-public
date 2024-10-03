38936f074e15535203af85696e7be1602bb9a16f
isofinly <isofinly@icloud.com>
2024-10-03 17:13:48 +0300

    uncomment

--- python/src/cli/main.py
+++ python/src/cli/main.py
@@ -33,12 +33,12 @@ class PredictionError(Exception):
         super().__init__(self.message)
 
 
-# @retry(
-#     stop=stop_after_attempt(3),
-#     wait=wait_exponential(multiplier=1, min=4, max=10),
-#     retry=retry_if_exception_type(PredictionError),
-#     reraise=True,
-# )
+@retry(
+    stop=stop_after_attempt(3),
+    wait=wait_exponential(multiplier=1, min=4, max=10),
+    retry=retry_if_exception_type(PredictionError),
+    reraise=True,
+)
 def make_prediction(
     model_registry: ModelRegistry, inputs: List[str], categories: Optional[List[str]]
 ) -> List[Dict[str, Any]]:
